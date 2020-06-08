7ec1a5b97b3937aa333875439c05f2c0244f9935
ysun0056 <ysun0056@student.monash.edu>
2020-06-08 14:48:02 +0800

    data query using php

---
+++ Animal_Sense_Latest_Final_Build/data_query/page-example.php
@@ -0,0 +1,39 @@
+<?php
+global $wpdb;
+
+//Get values from URL
+$center_lat = $_GET["lat"];
+$center_lng = $_GET["lng"];
+$radius = $_GET["radius"];
+
+// Start XML file, create parent node
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+//Database query 
+$rows = $wpdb->get_results("SELECT x.VernacularName, x.Latitude, x.Longitude,x.Address, ( 3959 * acos( cos( radians($center_lat) ) * cos( radians( x.Latitude ) ) * cos( radians( x.Longitude ) - radians($center_lng) ) + sin( radians($center_lat) ) * sin( radians( x.Latitude ) ) ) ) AS distance, x.Year, y.image as image FROM allanimals x JOIN animal_images y ON x.VernacularName = y.AnimalName HAVING distance < $radius ORDER BY distance LIMIT 0 , 20;");
+                  
+//Get number of rows in the result
+$rowCount = $wpdb->num_rows;
+
+//Check if retrieved row is greater than 0 then display values in table
+if($rowCount > 0)
+{
+                
+    header("Content-type: text/xml");
+    // Iterate through the rows, adding XML nodes for each
+    foreach($rows as $row){
+        $node = $dom->createElement("marker");
+        $newnode = $parnode->appendChild($node);
+        $newnode->setAttribute("name", $row->VernacularName);
+        $newnode->setAttribute("lat", $row->Latitude);
+        $newnode->setAttribute("lng", $row->Longitude);
+        $newnode->setAttribute("distance", $row->distance);
+        $newnode->setAttribute("image", $row->image);
+        $newnode->setAttribute("year", $row->Year);
+        $newnode->setAttribute("add", $row->Address);
+    }
+    echo $dom->saveXML();
+}
+?>
